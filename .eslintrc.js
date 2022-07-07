40fa727f0df6293f53da64f14dfe86c54337b649
Darlington <fritzdultimate@gmail.com>
2022-07-07 15:04:47 +0000

    initial setups

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,28 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
+    "@vue/prettier",
+    "plugin:prettier/recommended",
+  ],
   parserOptions: {
     parser: "babel-eslint",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": [
+      'error',
+      {
+        'endOfLine': 'auto',
+      }
+    ]
   },
   overrides: [
     {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
+      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
       env: {
         jest: true,
       },
