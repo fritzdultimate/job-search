d8fcffa9ec7a91e60a481a3562f7e54cee16c231
Darlington <fritzdultimate@gmail.com>
2022-07-07 15:46:32 +0000

    tailwind import

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,11 +16,11 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "prettier/prettier": [
-      'error',
+      "error",
       {
-        'endOfLine': 'auto',
-      }
-    ]
+        endOfLine: "auto",
+      },
+    ],
   },
   overrides: [
     {
