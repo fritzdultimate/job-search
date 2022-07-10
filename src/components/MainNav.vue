693289776fc454227f487e34205b2098b82a1ddf
Darlington <fritzdultimate@gmail.com>
2022-07-10 01:10:39 +0000

    cleanup

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -5,9 +5,6 @@
         class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-gray-1"
       >
         <a href="/" class="flex items-center h-full text-xl">{{ company }}</a>
-        <h2 class="flex items-center h-full ml-8">
-          Developed by {{ author.firstname }} {{ author.lastname }}
-        </h2>
       </div>
     </div>
   </header>
@@ -19,10 +16,6 @@ export default {
   data() {
     return {
       company: "Fritz Careers",
-      author: {
-        firstname: "Darlington",
-        lastname: "Nwosu",
-      },
     };
   },
 };
