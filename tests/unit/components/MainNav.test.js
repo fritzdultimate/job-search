16becf7f3773c861f46c91aa19ca5d43fbd216ed
Darlington <fritzdultimate@gmail.com>
2022-07-10 01:22:56 +0000

    componenet testiing intro

---
+++ tests/unit/components/MainNav.test.js
@@ -0,0 +1,8 @@
+import { mount } from "@vue/test-utils";
+import MainNav from "@/components/MainNav.vue";
+
+describe("MainNav", () => {
+  it("displays company name", () => {
+    const wrapper = mount(MainNav);
+  });
+});
