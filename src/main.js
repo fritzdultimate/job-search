a0f634efd62386b670f04efc23560179e3777801
Darlington <fritzdultimate@gmail.com>
2022-07-15 12:29:07 +0000

    importing fontawesome icon

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from "vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
 import App from "@/App.vue";
 import "@/assets/tailwind.css";
 
-createApp(App).mount("#app");
+library.add(faSearch);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
