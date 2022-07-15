233b23b675be6bc45cf39fd6244ae12af431e6c4
Darlington <fritzdultimate@gmail.com>
2022-07-15 10:28:25 +0000

    subnav cont

--- src/components/Subnav.vue
+++ src/components/Subnav.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>This is a subnav</div>
+  <div class="w-full h-16 bg-white border-b border-solid border-brand-gray-1">
+    <div class="flex items-center h-full px-8">This is a subnav</div>
+  </div>
 </template>
 
 <script>
