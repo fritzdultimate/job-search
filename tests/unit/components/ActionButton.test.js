a51f571b520149ad8dcb42e4952855500bf8b1fa
Darlington <fritzdultimate@gmail.com>
2022-07-15 00:00:56 +0000

    testing action button

---
+++ tests/unit/components/ActionButton.test.js
@@ -0,0 +1,25 @@
+import { mount } from "@vue/test-utils";
+
+import ActionButton from "@/components/ActionButton.vue";
+
+describe("ActionButton", () => {
+  it("it renders text", () => {
+    const wrapper = mount(ActionButton, {
+      props: {
+        text: "clickable",
+        type: "primary",
+      },
+    });
+    expect(wrapper.text()).toMatch("clickable");
+  });
+
+  it("applies on or several style to button", () => {
+    const wrapper = mount(ActionButton, {
+      props: {
+        type: "primary",
+      },
+    });
+    const button = wrapper.find("button");
+    expect(button.classes("primary")).toBe(true);
+  });
+});
