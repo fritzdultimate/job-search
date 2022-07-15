6facfc432367a72f9e44a9480493bb18b3929039
Darlington <fritzdultimate@gmail.com>
2022-07-15 14:13:01 +0000

    hero element

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <MainNav />
+  <div>
+    <main-nav />
+    <hero />
+  </div>
 </template>
 
 <script>
 import MainNav from "@/components/MainNav.vue";
+import Hero from "@/components/Hero.vue";
 
 export default {
   name: "App",
   components: {
     MainNav,
+    Hero,
   },
 };
 </script>
---
+++ src/components/Hero.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <section>I can be your hero baby!</section>
+  </main>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Hero",
+};
+</script>
