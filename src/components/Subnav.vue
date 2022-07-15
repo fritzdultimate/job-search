482a3512ca189bc041ff20018ccc3ecfa14cb7d6
Darlington <fritzdultimate@gmail.com>
2022-07-15 12:40:20 +0000

    subnav

--- src/components/Subnav.vue
+++ src/components/Subnav.vue
@@ -3,6 +3,7 @@
     <div class="flex items-center h-full px-8">
       <div>
         <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
+        <span> <span class="text-brand-green-1">1653</span> jobs matched </span>
       </div>
     </div>
   </div>
