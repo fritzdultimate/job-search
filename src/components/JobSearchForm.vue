ff735b8e0c5300922d10ec096f610ca978d2ae9d
Darlington <fritzdultimate@gmail.com>
2022-07-16 13:46:19 +0000

    job search form

--- src/components/JobSearchForm.vue
+++ src/components/JobSearchForm.vue
@@ -1,5 +1,9 @@
 <template>
-  <form>I will be in a better form eventually</form>
+  <form
+    class="flex items-center w-full h-12 mt-14 border border-solid border-brand-gray-3 rounded-3xl"
+  >
+    <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3" />
+  </form>
 </template>
 
 <script>
