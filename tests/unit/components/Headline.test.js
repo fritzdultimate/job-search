85d6852280df9f958189d02d1e6ce8c75797b1ea
Darlington <fritzdultimate@gmail.com>
2022-07-16 12:24:40 +0000

    dry test

--- tests/unit/components/Headline.test.js
+++ tests/unit/components/Headline.test.js
@@ -4,37 +4,36 @@ import { mount } from "@vue/test-utils";
 import Headline from "@/components/Headline";
 
 describe("Headline", () => {
+  beforeEach(() => {
+    jest.useFakeTimers("legacy");
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
   it("displays introductory action verbs", () => {
-    jest.useFakeTimers();
     const wrapper = mount(Headline);
     const actionPhrase = wrapper.find("[data-test='action-phrase']");
     expect(actionPhrase.text()).toBe("Build for everyone");
-    jest.useRealTimers();
   });
 
   it("changes action verb at a consistent interval", () => {
-    jest.useFakeTimers("legacy");
     mount(Headline);
     expect(setInterval).toHaveBeenCalled();
-    jest.useRealTimers();
   });
 
   it("swaps action verb after first interval", async () => {
-    jest.useFakeTimers();
     const wrapper = mount(Headline);
     jest.runOnlyPendingTimers();
     await nextTick();
     const actionPhrase = wrapper.find("[data-test='action-phrase']");
     expect(actionPhrase.text()).toBe("Create for everyone");
-
-    jest.useRealTimers();
   });
 
   it("removes interval when component disappears", () => {
-    jest.useFakeTimers("legacy");
     const wrapper = mount(Headline);
     wrapper.unmount();
     expect(clearInterval).toHaveBeenCalled();
-    jest.useRealTimers();
   });
 });
