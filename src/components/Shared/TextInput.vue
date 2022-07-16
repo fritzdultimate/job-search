c5dd95c38db2e86786a228a1504409f60820d15b
Darlington <fritzdultimate@gmail.com>
2022-07-16 20:31:35 +0000

    refactoring text input

---
+++ src/components/Shared/TextInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <input
+    :value="value"
+    type="text"
+    :placeholder="placeholder"
+    class="w-full text-lg font-normal focus:outline-none"
+    @input="handleInput"
+  />
+</template>
+
+<script>
+export default {
+  name: "TextInput",
+  props: {
+    placeholder: {
+      type: String,
+      required: false,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      value: "",
+    };
+  },
+  methods: {
+    handleInput($event) {
+      this.value = $event.target.value;
+    },
+  },
+};
+</script>
