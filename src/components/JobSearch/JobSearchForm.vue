6965e0b2072749f7c073af75b923700fee283034
Darlington <fritzdultimate@gmail.com>
2022-07-19 18:59:09 +0000

    emit

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -7,12 +7,7 @@
     <div class="flex flex-nowrap flex-1 h-full text-base font-light">
       <div class="relative flex items-center flex-1 h-full pr-3">
         <label class="absolute left-0 -top-10">Role</label>
-        <input
-          v-model="role"
-          type="text"
-          placeholder="Software engineer"
-          class="w-full text-lg font-normal focus:outline-none"
-        />
+        <text-input placeholder="Software engineer" />
       </div>
       <span
         class="flex items-center h-full px-3 border-l border-r border-brand-gray-3 bg-brand-gray-2"
@@ -21,12 +16,7 @@
 
       <div class="relative flex items-center flex-1 h-full pl-3">
         <label class="absolute left-0 -top-10">Where</label>
-        <input
-          v-model="location"
-          type="text"
-          placeholder="Awka, Anambra"
-          class="w-full text-lg font-normal focus:outline-none"
-        />
+        <text-input placeholder="Awka, Anambra" />
       </div>
     </div>
 
@@ -36,10 +26,12 @@
 
 <script>
 import ActionButton from "@/components/Shared/ActionButton";
+import TextInput from "@/components/Shared/TextInput";
 export default {
   name: "JobSearchForn",
   components: {
     ActionButton,
+    TextInput,
   },
   data() {
     return {
--- src/components/Shared/TextInput.vue
+++ src/components/Shared/TextInput.vue
@@ -26,6 +26,7 @@ export default {
   methods: {
     handleInput($event) {
       this.value = $event.target.value;
+      this.$$emit("handleInput", this.value);
     },
   },
 };
