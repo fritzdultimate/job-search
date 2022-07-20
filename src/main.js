ebf1bf5629478f3ac3ededbc72d2483ba2ec19a6
Darlington <fritzdultimate@gmail.com>
2022-07-20 23:55:17 +0000

    install vue router

--- src/main.js
+++ src/main.js
@@ -5,7 +5,11 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 import App from "@/App.vue";
 import "@/assets/tailwind.css";
+import router from "@/router";
 
 library.add(faSearch);
 
-createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
+createApp(App)
+  .use(router)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
