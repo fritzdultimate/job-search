94b071226cf31a197a06022f740e241ca0e0813a
Darlington <fritzdultimate@gmail.com>
2022-07-20 23:09:55 +0000

    textinput

--- src/components/Shared/TextInput.vue
+++ src/components/Shared/TextInput.vue
@@ -17,16 +17,14 @@ export default {
       required: false,
       default: "",
     },
-  },
-  data() {
-    return {
-      value: "",
-    };
+    value: {
+      type: String,
+      required: true,
+    },
   },
   methods: {
     handleInput($event) {
-      this.value = $event.target.value;
-      this.$emit("handleInput", this.value);
+      this.$emit("handleInput", $event.target.value);
     },
   },
 };
