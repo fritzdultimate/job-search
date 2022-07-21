5b4adf884b16ca71766e007f164908281c543ad7
Darlington <fritzdultimate@gmail.com>
2022-07-21 00:45:03 +0000

    $route

--- src/views/JobResultsView.vue
+++ src/views/JobResultsView.vue
@@ -1,9 +1,16 @@
 <template>
   <h1 class="text-6xl font-bold">JobResults View</h1>
+  <h2>{{ experiment }}</h2>
 </template>
 
 <script>
 export default {
   name: "JobResultsView",
+  computed: {
+    experiment() {
+      console.log(this.$route);
+      return "Irrelevant";
+    },
+  },
 };
 </script>
