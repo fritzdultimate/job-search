dee4d6bd7fea1765419966bc3bacbc2ba6d84314
Darlington <fritzdultimate@gmail.com>
2022-07-22 22:11:31 +0000

    dynamic routing

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,9 +1,14 @@
 <template>
-  <div>Job Page</div>
+  <div>Job Page {{ currentJobId }}</div>
 </template>
 
 <script>
 export default {
   name: "JobView",
+  computed: {
+    currentJobId() {
+      return this.$route.params.id;
+    },
+  },
 };
 </script>
