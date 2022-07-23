ffab2bcd37a57cce2b003e3d18c0ea6b6454ef6e
Darlington <fritzdultimate@gmail.com>
2022-07-23 23:56:51 +0100

    testing

--- tests/unit/components/JobResults/JobListing.test.js
+++ tests/unit/components/JobResults/JobListing.test.js
@@ -3,35 +3,26 @@ import { mount, RouterLinkStub } from "@vue/test-utils";
 import JobListing from "@/components/JobResults/JobListing.vue";
 
 describe("JobListing", () => {
+  const createConfig = () => ({
+    props: {
+      job: {
+        title: "Vue Developer",
+        organization: "C2GEN",
+      },
+    },
+    global: {
+      stubs: {
+        "router-link": RouterLinkStub,
+      },
+    },
+  });
   it("renders job title", () => {
-    const wrapper = mount(JobListing, {
-      props: {
-        job: {
-          title: "Vue Developer",
-        },
-      },
-      global: {
-        stubs: {
-          "router-link": RouterLinkStub,
-        },
-      },
-    });
+    const wrapper = mount(JobListing, createConfig());
     expect(wrapper.text()).toMatch("Vue Developer");
   });
 
   it("renders job organization", () => {
-    const wrapper = mount(JobListing, {
-      props: {
-        job: {
-          organization: "C2GEN",
-        },
-      },
-      global: {
-        stubs: {
-          "router-link": RouterLinkStub,
-        },
-      },
-    });
+    const wrapper = mount(JobListing, createConfig());
     expect(wrapper.text()).toMatch("C2GEN");
   });
 });
