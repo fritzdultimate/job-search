424c55e02e19848dd6369608558d727bcc55ed29
Darlington <fritzdultimate@gmail.com>
2022-07-23 23:31:02 +0100

    dynamic linking

--- src/components/JobResults/JobListing.vue
+++ src/components/JobResults/JobListing.vue
@@ -1,7 +1,7 @@
 <template>
   <li class="mb-7">
     <router-link
-      to="/jobs/results/1"
+      :to="jobPageLink"
       class="block mx-auto bg-white border border-solid border-brand-gray-2 rounded hover:shadow-gray"
     >
       <div class="pt-5 pb-2 mx-8 border-b border-solid border-brand-gray-2">
