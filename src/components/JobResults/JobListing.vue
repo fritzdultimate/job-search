ca8aa3cf7ee9edc2476a230c3dae94e139ab99e5
Darlington <fritzdultimate@gmail.com>
2022-07-24 00:21:02 +0100

    job-page-link dataset

--- src/components/JobResults/JobListing.vue
+++ src/components/JobResults/JobListing.vue
@@ -3,6 +3,7 @@
     <router-link
       :to="jobPageLink"
       class="block mx-auto bg-white border border-solid border-brand-gray-2 rounded hover:shadow-gray"
+      data-test="job-page-link"
     >
       <div class="pt-5 pb-2 mx-8 border-b border-solid border-brand-gray-2">
         <h2 class="mb-2 text-2xl">
