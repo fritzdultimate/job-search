0f1827730c2d81e48958c902a6272cf9a41b502c
Darlington <fritzdultimate@gmail.com>
2022-07-23 20:29:41 +0000

    joblisting

---
+++ src/components/JobResults/JobListings.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>Jobs listings component</main>
+</template>
+
+<script>
+export default {
+  name: "JobListings",
+};
+</script>
--- src/views/JobResultsView.vue
+++ src/views/JobResultsView.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <job-filter-sidebar />
+    <job-listings />
   </div>
 </template>
 
 <script>
 import JobFilterSidebar from "@/components/JobResults/JobFilterSidebar/JobFilterSidebar.vue";
+import JobListings from "@/components/JobResults/JobListings.vue";
 export default {
   name: "JobResultsView",
   components: {
     JobFilterSidebar,
+    JobListings,
   },
 };
 </script>
