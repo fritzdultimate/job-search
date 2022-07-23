8df1cf17233e7192e6ab8829cc4e6f09c980d489
Darlington <fritzdultimate@gmail.com>
2022-07-23 23:47:30 +0100

    testing joblisting

--- tests/unit/components/JobResults/JobListing.test.js
+++ tests/unit/components/JobResults/JobListing.test.js
@@ -2,4 +2,15 @@ import { mount } from "@vue/test-utils";
 
 import JobListing from "@/components/JobResults/JobListing.vue";
 
-describe("JobListing", () => {})
+describe("JobListing", () => {
+  it("renders job title", () => {
+    const wrapper = mount(JobListing, {
+      props: {
+        job: {
+          title: "Vue Developer",
+        },
+      },
+    });
+    expect(wrapper.text()).toMatch("Vue Developer");
+  });
+});
