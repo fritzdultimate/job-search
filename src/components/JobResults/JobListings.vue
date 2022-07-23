5c440b1ce99248bd444a6a312cc84dc6dc67797d
Darlington <fritzdultimate@gmail.com>
2022-07-23 23:20:31 +0100

    axios in action

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -20,10 +20,9 @@ export default {
       jobs: [],
     };
   },
-  mounted() {
-    axios.get("http://localhost:3000/jobs").then((response) => {
-      this.jobs = response.data;
-    });
+  async mounted() {
+    const response = await axios.get("http://localhost:3000/jobs");
+    this.jobs = response.data;
   },
 };
 </script>
