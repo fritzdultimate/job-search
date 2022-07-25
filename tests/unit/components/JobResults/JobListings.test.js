f59bc2e155ef71182f7f5162bf9b4418178f45e0
Darlington <fritzdultimate@gmail.com>
2022-07-25 20:57:05 +0100

    test

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -2,7 +2,7 @@ import axios from "axios";
 jest.mock("axios");
 
 import JobListings from "@/components/JobResults/JobListings.vue";
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, flushPromises } from "@vue/test-utils";
 
 describe("JobListings", () => {
   it("fetches jobs", () => {
@@ -11,9 +11,10 @@ describe("JobListings", () => {
     expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
   });
 
-  it("creates a job listing for each received job", () => {
+  it("creates a job listing for each received job", async () => {
     axios.get.mockResolvedValue({ data: Array(15).fill({}) });
     const wrapper = shallowMount(JobListings);
+    await flushPromises();
     const jobListings = wrapper.findAll("[data-test='job-listing']");
     expect(jobListings).toHaveLength(15);
   });
