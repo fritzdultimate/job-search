7e8fd6fed9c23ab7f18ef2b091b22c0d869a14a9
Darlington <fritzdultimate@gmail.com>
2022-07-25 20:39:15 +0100

    job listing test

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -1,2 +1,18 @@
 import axios from "axios";
 jest.mock("axios");
+
+import JobListings from "@/components/JobResults/JobListings.vue";
+import { shallowMount } from "@vue/test-utils";
+
+describe("JobListings", () => {
+  it("fetches jobs", () => {
+    axios.get.mockResolvedValue({ data: [] });
+    shallowMount(JobListings);
+    expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
+  });
+
+  it("creates a job listing for each received job", () => {
+    axios.get.mockResolvedValue({ data: [] });
+    const wrapper = shallowMount(JobListings);
+  });
+});
