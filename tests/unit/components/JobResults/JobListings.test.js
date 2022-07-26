46bca12e9f4998a4742fd3e4845398956e8a4c0b
Darlington <fritzdultimate@gmail.com>
2022-07-26 23:18:19 +0100

    test

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -11,11 +11,11 @@ describe("JobListings", () => {
     expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
   });
 
-  it("creates a job listing for each received job", async () => {
+  it("creates a job listing for a maximum of ten job", async () => {
     axios.get.mockResolvedValue({ data: Array(15).fill({}) });
     const wrapper = shallowMount(JobListings);
     await flushPromises();
     const jobListings = wrapper.findAll("[data-test='job-listing']");
-    expect(jobListings).toHaveLength(15);
+    expect(jobListings).toHaveLength(10);
   });
 });
