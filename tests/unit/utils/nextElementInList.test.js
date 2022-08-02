e47c15ec766f9002928e8aadbff84049c7e49f25
Darlington <59008938+fritzdultimate@users.noreply.github.com>
2022-08-02 23:56:07 +0100

    Update nextElementInList.test.js

--- tests/unit/utils/nextElementInList.test.js
+++ tests/unit/utils/nextElementInList.test.js
@@ -2,7 +2,7 @@ import nextElementInList from "@/utils/nextElementInList";
 
 describe("nextElementInList", () => {
   it("locates element in list and returns the next element in list", () => {
-    const list = ["A", "B", "C", "D"];
+    const list = ["A", "B", "C", "D", "E"];
     const value = "B";
     const result = nextElementInList(list, value);
     expect(result).toBe("C");
